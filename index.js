836c0a3a773b3e1a1fb28c559f830bc011cad67a
mohamedelgallad-510 <mohamed.elgallad9@gmail.com>
2026-01-02 21:43:36 +0200

    Clean up index.js by removing unused code
    Removed console log and commented challenge instructions.

--- index.js
+++ index.js
@@ -37,17 +37,5 @@ function getFeedHtml(){
         </div>`
     }
     
-    console.log(feedHtml)
-/*
-Challenge:
-1. Use a "for of" to iterate over the data and 
-   create HTML string for each tweet using the 
-   boilerplate below. Replace UPPERCASE text
-   with data from the tweets. 
-2. Store this HTML in a let called "feedHtml".
-3. Log out feedHtml.
-4. Call getFeedHtml to check it's working.
-*/  
+    return feedHtml 
 }
-
-getFeedHtml()
