6bf608351d8ab1ca9a38da13a8deeba59ebbb339
mohamedelgallad-510 <mohamed.elgallad9@gmail.com>
2026-01-03 23:05:17 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,19 +1,18 @@
-/*
-Challenge:
-1. Put all of the data in its own file called
-   data.js, and export it back into index.js. 
-   Make any changes to index.html that are
-   necessary to make this work.
-2. Log out tweetsData.
-*/
+import { tweetsData } from './data.js'
+const tweetInput = document.getElementById('tweet-input')
+const tweetBtn = document.getElementById('tweet-btn')
 
-import {tweetsData} from '/data.js'
+tweetBtn.addEventListener('click', function(){
+    console.log(tweetInput.value)
+})
 
-console.log(tweetsData)
+document.addEventListener('click', function(e){
+    console.log(e.target.dataset.like)
+})
 
 function getFeedHtml(){
-   
     let feedHtml = ``
+    
     tweetsData.forEach(function(tweet){
         feedHtml += `
 <div class="tweet">
@@ -24,15 +23,21 @@ function getFeedHtml(){
             <p class="tweet-text">${tweet.tweetText}</p>
             <div class="tweet-details">
                 <span class="tweet-detail">
-                    <i class="fa-regular fa-comment-dots" data-reply="${tweet.uuid}"></i>
+                    <i class="fa-regular fa-comment-dots"
+                    data-reply="${tweet.uuid}"
+                    ></i>
                     ${tweet.replies.length}
                 </span>
                 <span class="tweet-detail">
-                    <i class="fa-solid fa-heart" data-like="${tweet.uuid}"></i>
+                    <i class="fa-solid fa-heart"
+                    data-like="${tweet.uuid}"
+                    ></i>
                     ${tweet.likes}
                 </span>
                 <span class="tweet-detail">
-                    <i class="fa-solid fa-retweet" data-retweet="${tweet.uuid}"></i>
+                    <i class="fa-solid fa-retweet"
+                    data-retweet="${tweet.uuid}"
+                    ></i>
                     ${tweet.retweets}
                 </span>
             </div>   
