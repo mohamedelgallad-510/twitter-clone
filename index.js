cf4126942bff113eaa80aafe9ad24688496e4ed4
mohamedelgallad-510 <mohamed.elgallad9@gmail.com>
2026-01-07 22:51:29 +0300

    Prevented empty tweets and cleared the tweet text area after tweeting

--- index.js
+++ index.js
@@ -84,19 +84,21 @@ function handleReplyClick(replyId){
 }
 
 function handleTweetBtnClick(){
-    tweetsData.unshift({
-        handle: `@Scrimba`,
-        profilePic: `images/scrimbalogo.png`,
-        likes: 0,
-        retweets: 0,
-        tweetText: tweetInput.value,
-        replies: [],
-        isLiked: false,
-        isRetweeted: false,
-        uuid: uuidv4()
-    })
-    
+    if(tweetInput.value){
+        tweetsData.unshift({
+            handle: `@Scrimba`,
+            profilePic: `images/scrimbalogo.png`,
+            likes: 0,
+            retweets: 0,
+            tweetText: tweetInput.value,
+            replies: [],
+            isLiked: false,
+            isRetweeted: false,
+            uuid: uuidv4()
+        })
+    }
     render()
+    tweetInput.value = ""
 }
 
 function getFeedHtml(){
